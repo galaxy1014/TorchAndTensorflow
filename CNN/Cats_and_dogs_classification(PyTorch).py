f52a382668ca34b07a3d7a3f6dacac542fba4499
galaxy1014 <galaxy1014@naver.com>
2021-02-19 19:37:19 +0900

    update : torch cats and dogs
    update torch cats and dogs

--- CNN/Cats_and_dogs_classification(PyTorch).py
+++ CNN/Cats_and_dogs_classification(PyTorch).py
@@ -9,14 +9,13 @@ from torchvision import transforms
 # Train and Test Data
 compose = transforms.Compose([transforms.Resize((150, 150)),
                                transforms.ToTensor(),
-                              transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                                   std=[0.229, 0.224, 0.225])])
+                              transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])])
 train_data = ImageFolder(root = 'Cats_and_dogs/train',
                                              transform = compose)
 test_data = ImageFolder(root = 'Cats_and_dogs/validation',
                                             transform = compose)
 
-der = DataLoader(train_data, batch_size = 64, shuffle=True, drop_last=True)
+train_loader = DataLoader(train_data, batch_size = 64, shuffle=True, drop_last=True)
 test_loader = DataLoader(test_data, batch_size = 64, shuffle=True)
 
 # Model
@@ -30,14 +29,14 @@ class DeepCNN(nn.Module):
             nn.MaxPool2d(2),
             nn.BatchNorm2d(32)
         )
-        
+
         self.conv2 = nn.Sequential(
             nn.Conv2d(32, 64, 3, 1),
             nn.ReLU(),
             nn.MaxPool2d(2),
             nn.BatchNorm2d(64)
         )
-        
+
         self.dropout = nn.Dropout(0.3)
         self.fc1 = nn.Sequential(
             nn.Linear(36*36*64, 128),
@@ -47,42 +46,41 @@ class DeepCNN(nn.Module):
             nn.Linear(128, 64),
             nn.ReLU()
         )
-        
+
         self.output = nn.Linear(64, 1)
-        torch.nn.init.xavier_uniform_(self.output.weight)
 
     def forward(self, x):
         out = self.conv1(x)
         out = self.conv2(out)
         out = self.dropout(out)
-        out = out.view(-1, 36*36*64)
+        # out = out.view(-1, 36*36*64)
+        out = out.view(out.size(0), -1)
         out = self.fc1(out)
         out = self.fc2(out)
         out = self.output(out)
-        out = torch.sigmoid(out)
         return out
 
 model = DeepCNN().to('cuda:0')
-criterion = nn.BCELoss()
-optimizer = optim.SGD(model.parameters(), lr = 0.001, momentum=0.9)
+criterion = nn.BCEWithLogitsLoss()
+optimizer = optim.SGD(model.parameters(), lr = 3e-5, momentum=0.9)
 
 from torchsummary import summary
 summary(model, (3, 150, 150))
 
 # Training
 
-for epoch in range(200):
+for epoch in range(180):
     for X, Y in train_loader:
         X = X.to('cuda:0')
         Y = Y.to('cuda:0').float()
-        
+
         optimizer.zero_grad()
         prediction = model(X).to('cuda:0')
         Y = Y.unsqueeze(1)
         cost = criterion(prediction, Y).to('cuda:0')
         cost.backward()
         optimizer.step()
-        
+
     print('Epoch: {}, cost : {:.6f}'.format(epoch + 1,cost.item()))
 
 # Testing
@@ -97,10 +95,10 @@ with torch.no_grad():
     print('Accuracy:', accuracy.item())
 
 
-from PIL import Image 
-from matplotlib.pyplot import imshow 
+from PIL import Image
+from matplotlib.pyplot import imshow
 import numpy as np
-img = Image.open('sample_image.jpg') 
+img = Image.open('sample_image.jpg')
 imshow(np.asarray(img))
 
 
@@ -111,14 +109,10 @@ def custom_image(img):
     return image
 
 def run():
-    img = Image.open('sample_image.jpg') 
+    img = Image.open('sample_image.jpg')
     img = custom_image(img)
     result = model(img).to('cuda:0')
     _, predicted = torch.max(result, 1)
     return predicted
 
 print(run())
-
-
-
-
